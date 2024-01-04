484d81366806034aa6a5fcc5a1bb2534885beacd
123 <154058106+huaize111@users.noreply.github.com>
2024-01-04 10:25:11 +0000

    Update 10 files

---
+++ config.js
@@ -0,0 +1,84 @@
+/*
+ * https://rentry.org/teralomaniac_clewd
+ * https://github.com/teralomaniac/clewd
+ */
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+  Cookie: "SET YOUR COOKIE HERE",
+  CookieArray: [
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+    "sessionKey=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",
+  ],
+  Cookiecounter: 3,
+  CookieIndex: 0,
+  ProxyPassword: "13505037231",
+  Ip: "127.0.0.1",
+  Port: 8444,
+  localtunnel: false,
+  BufferSize: 1,
+  SystemInterval: 3,
+  rProxy: "",
+  api_rProxy: "",
+  padtxt_placeholder: "",
+  PromptExperimentFirst: "",
+  PromptExperimentNext: "",
+  PersonalityFormat: "{{char}}'s personality: {{personality}}",
+  ScenarioFormat: "Dialogue scenario: {{scenario}}",
+  Settings: {
+    RenewAlways: true,
+    RetryRegenerate: false,
+    PromptExperiments: true,
+    SystemExperiments: true,
+    PreventImperson: false,
+    AllSamples: false,
+    NoSamples: false,
+    StripAssistant: false,
+    StripHuman: false,
+    PassParams: false,
+    ClearFlags: true,
+    PreserveChats: false,
+    LogMessages: true,
+    FullColon: true,
+    padtxt: 15000,
+    xmlPlot: true,
+    Superfetch: true,
+  },
+};
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
